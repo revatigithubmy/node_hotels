71991c1a42984efd48f512fd56d1cc10ec63d513
revatigithubmy <revatishimpi1955@gmail.com>
2025-08-19 18:34:00 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 //define the MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels'// Replace 'hotel' with your database name
+//const mongoURL = 'mongodb://localhost:27017/hotels'// Replace 'hotel' with your database name
+const mongoURL =process.env.MONGODB_URL;
+
+//set up mongodb connection
 mongoose.connect(mongoURL,{
   useNewUrlParser:true,
   useUnifiedTopology:true
 })
+
+mongoose.connect(mongoURL, {
+  ssl: true,
+  tlsAllowInvalidCertificates: false, // Only for development/testing
+  // Other options...
+});
 
 //get the default connection
 //Mongoose maintains a default connection object representing the MongoDB connection.
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const db= require('./db');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
-const { error } = require('console');
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send('Welcome to my hotel... How i can help you?, we have list of menus')
@@ -20,6 +21,8 @@ app.use('/person',personRoutes);
 const menuRoutes = require('./routes/menuRoutes');
 //use the router
 app.use('/MenuItem',menuRoutes);
+
+
 
 app.listen(3000,()=>{
   console.log('Listening on port 3000');
