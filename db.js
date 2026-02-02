16116cf608d1c47d56536bed7c26ed6ed3eefa38
Revati Shimpi <revatishimpi1955@gmail.com>
2026-02-02 16:04:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-//define the MongoDB connection URL
-//const mongoURL = 'mongodb://localhost:27017/hotels'// Replace 'hotel' with your database name
+
 const mongoURL =process.env.MONGODB_URL;
 
 //set up mongodb connection
